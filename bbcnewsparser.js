913fa722fd0b478402e0360ef4529beeeec0833d
jamesfmackenzie <jim@lilandjim.com>
2017-05-02 23:22:33 +0100

    Fix parser after changes to bbcnews site

--- bbcnewsparser.js
+++ bbcnewsparser.js
@@ -1,50 +1,68 @@
 // bbcnewsparser.js
 var request,
-htmlparser = require('htmlparser2');
+	htmlparser = require('htmlparser2');
 
 var NewsParser = function (request) {
 	this.request = request;
 };
 
 NewsParser.prototype.parseMostReadArticles = function (callback) {
-  this.request('http://www.bbc.co.uk/news/popular/read', function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	  	callback(parseMostReadArticlesFromHtml(body));
-      }
-	  else {
-		console.error(response, error);
-        callback([]);
-	  }
-    });
+	this.request('http://www.bbc.co.uk/news/0', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			//console.log(body);
+			callback(parseMostReadArticlesFromHtml(body));
+		} else {
+			console.error(response, error);
+			callback([]);
+		}
+	});
 }
 
 function parseMostReadArticlesFromHtml(html) {
 	var parsedArticles = [],
-	linkText = "",
-	insideHeadlineTag = false,
-	parser = new htmlparser.Parser({
-   		onopentag: function(name, attribs){
-       		if(name === "a" && attribs.class === "most-popular-page-list-item__link"){
-          			parsedArticles.push(attribs.href);
-       		}
-			if (name == "span" && attribs.class === "most-popular-page-list-item__headline") {
-				insideHeadlineTag = true;	
+		linkText = "",
+		insideMostReadSection = false,
+		insideHeadlineSection = false,
+		tagCount = 0,
+		parser = new htmlparser.Parser({
+			onopentag: function (name, attribs) {
+				if (name == "div" && attribs.class && attribs.class.includes("nw-c-most-read__items")) {
+					insideMostReadSection = true;
+				}
+				if (insideMostReadSection) {
+					tagCount++;
+				}
+				if (insideMostReadSection && name === "a") {
+					insideHeadlineSection = true;
+					parsedArticles.push(attribs.href);
+				}
+
+			},
+			ontext: function (text) {
+				if (insideHeadlineSection) {
+					linkText = linkText + text;
+				}
+			},
+			onclosetag: function (tagname) {
+				if (insideMostReadSection) {
+					tagCount--;
+				}
+				if (tagCount <= 0) {
+					insideMostReadSection = false;
+				}
+				if (insideHeadlineSection && tagname === "a") {
+					var url = parsedArticles.pop();
+					parsedArticles.push({
+						url: 'http://www.bbc.co.uk' + url,
+						title: linkText.replace(/\s\s+/g, ' ').trim()
+					});
+					linkText = "";
+					insideHeadlineSection = false;
+				}
 			}
-   		},
-   		ontext: function(text){
-			if (insideHeadlineTag) {
-				linkText = linkText + text;
-			}
-   		},
-   		onclosetag: function(tagname){
-			if (insideHeadlineTag && tagname === "span") {
-				var url = parsedArticles.pop();
-				parsedArticles.push({ url: 'http://www.bbc.co.uk' + url, title: linkText.replace(/\s\s+/g, ' ').trim() });
-				linkText = "";
-				insideHeadlineTag = false;
-			}	
-       	}
-	}, {decodeEntities: true});
+		}, {
+			decodeEntities: true
+		});
 	parser.write(html);
 	parser.end();
 
--- spec/unit/bbcnewsparser.spec.js
+++ spec/unit/bbcnewsparser.spec.js
@@ -1,109 +1,109 @@
-describe("NewsParser", function() {
-  var NewsParser = require("../../bbcnewsparser.js");  
-    
-  describe("parseMostReadArticles", function() {
-	it("known body, returns expected result", function(done) {
-	  // arrange 
-	  var requestWithKnownValidResponse = function(url, callback) {
-		var error = null,
-		  response = { statusCode: 200 },
-		  body = `
-              <div id="comp-most-popular-page" class="most-popular-page">
-                  <h1 class="most-popular-page__title">Most Read</h1>
-                  <ul class="most-popular-page__list">
-                      <li class="most-popular-page-list-item">
-                          <a href="/news/uk-38663322" class="most-popular-page-list-item__link">
-                              <span class="most-popular-page-list-item__rank">1</span>
-                              <span class="most-popular-page-list-item__headline">'Wheelchair v buggy': Disabled man wins Supreme Court case</span>
-                          </a>
-                      </li>
-                      <li class="most-popular-page-list-item">
-                          <a href="/news/uk-england-38663331" class="most-popular-page-list-item__link">
-                              <span class="most-popular-page-list-item__rank">2</span>
-                              <span class="most-popular-page-list-item__headline">Southern rail 'full service' to resume next week</span>
-                          </a>
-                      </li>
-                      <li class="most-popular-page-list-item">
-                          <a href="/news/world-us-canada-38656271" class="most-popular-page-list-item__link">
-                              <span class="most-popular-page-list-item__rank">3</span>
-                              <span class="most-popular-page-list-item__headline">Trump inauguration boycott escalates</span>
-                          </a>
-                      </li>
-                  </ul>
-              </div>
+describe("NewsParser", function () {
+	var NewsParser = require("../../bbcnewsparser.js");
+
+	describe("parseMostReadArticles", function () {
+		it("known body, returns expected result", function (done) {
+			// arrange 
+			var requestWithKnownValidResponse = function (url, callback) {
+				var error = null,
+					response = {
+						statusCode: 200
+					},
+					body = `
+<div class="nw-c-most-read__items gel-layout gel-layout--no-flex">
+    <ol class="gel-layout__item">
+        <li data-entityid="most-popular-read-1" class="gel-layout__item gs-o-faux-block-link gs-u-mb+ gel-1/2@m gel-1/5@xxl gs-u-float-left@m gs-u-clear-left@m gs-u-float-none@xxl"><span class="gs-o-media"><span class="nw-c-most-read__rank gs-o-media__img gel-canon gel-1/12@xs gel-1/8@m gel-1/10@l gel-2/12@xxl gs-u-align-center">1</span>
+            <a class="gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs gs-o-media__body"
+                href="/news/uk-39779256"><span class="gs-c-promo-heading__title gel-pica-bold">Madeleine questioning shocked suspect</span></a>
+            </span>
+        </li>
+        <li data-entityid="most-popular-read-2" class="gel-layout__item gs-o-faux-block-link gs-u-mb+ gel-1/2@m gel-1/5@xxl gs-u-float-left@m gs-u-clear-left@m gs-u-float-none@xxl"><span class="gs-o-media"><span class="nw-c-most-read__rank gs-o-media__img gel-canon gel-1/12@xs gel-1/8@m gel-1/10@l gel-2/12@xxl gs-u-align-center">2</span>
+            <a class="gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs gs-o-media__body"
+                href="/news/uk-39781013"><span class="gs-c-promo-heading__title gel-pica-bold">Six on trial over topless duchess photos</span></a>
+            </span>
+        </li>
+        <li data-entityid="most-popular-read-3" class="gel-layout__item gs-o-faux-block-link gs-u-mb+ gel-1/2@m gel-1/5@xxl gs-u-float-left@m gs-u-clear-left@m gs-u-float-none@xxl"><span class="gs-o-media"><span class="nw-c-most-read__rank gs-o-media__img gel-canon gel-1/12@xs gel-1/8@m gel-1/10@l gel-2/12@xxl gs-u-align-center">3</span>
+            <a class="gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs gs-o-media__body"
+                href="/news/uk-politics-39784170"><span class="gs-c-promo-heading__title gel-pica-bold">PM: I&#x27;ll be bloody difficult to Juncker</span></a>
+            </span>
+        </li>
+    </ol>
+</div>
 		  `;
-		callback(error, response, body);
-	  }
-	  
-	  var newsParser = new NewsParser(requestWithKnownValidResponse);
-	  
-	  // act
-	  newsParser.parseMostReadArticles(function(result) {
-		  
-		// assert
-  		expect(result.length).toEqual(3);
-		expect(result[0].url).toEqual("http://www.bbc.co.uk/news/uk-38663322");
-		expect(result[0].title).toEqual("'Wheelchair v buggy': Disabled man wins Supreme Court case");
-		expect(result[1].url).toEqual("http://www.bbc.co.uk/news/uk-england-38663331");
-		expect(result[1].title).toEqual("Southern rail 'full service' to resume next week");
-		expect(result[2].url).toEqual("http://www.bbc.co.uk/news/world-us-canada-38656271");
-		expect(result[2].title).toEqual("Trump inauguration boycott escalates");
-		done();
-	  });
-    });  
-	
-    it("response with ampersand, returns expected result", function(done) {
-	  // arrange 
-	  var requestThatAlwaysReturnsExactlyOneResultWithAmpersandInTitle = function(url, callback) {
-		var error = null,
-		  response = { statusCode: 200 },
-		  body = `
-              <div id="comp-most-popular-page" class="most-popular-page">
-                  <h1 class="most-popular-page__title">Most Read</h1>
-                  <ul class="most-popular-page__list">
-                      <li class="most-popular-page-list-item">
-                          <a href="/news/blogs-the-papers-38565081" class="most-popular-page-list-item__link">
-                              <span class="most-popular-page-list-item__rank">1</span>
-                              <span class="most-popular-page-list-item__headline">Newspaper headlines: 'Broken' A&E and Streep takes on Trump</span>
-                          </a>
-                      </li>
-                  </ul>
-              </div>
+				callback(error, response, body);
+			}
+
+			var newsParser = new NewsParser(requestWithKnownValidResponse);
+
+			// act
+			newsParser.parseMostReadArticles(function (result) {
+
+				// assert
+				expect(result.length).toEqual(3);
+				expect(result[0].url).toEqual("http://www.bbc.co.uk/news/uk-39779256");
+				expect(result[0].title).toEqual("Madeleine questioning shocked suspect");
+				expect(result[1].url).toEqual("http://www.bbc.co.uk/news/uk-39781013");
+				expect(result[1].title).toEqual("Six on trial over topless duchess photos");
+				expect(result[2].url).toEqual("http://www.bbc.co.uk/news/uk-politics-39784170");
+				expect(result[2].title).toEqual("PM: I'll be bloody difficult to Juncker");
+				done();
+			});
+		});
+
+		it("response with ampersand, returns expected result", function (done) {
+			// arrange 
+			var requestThatAlwaysReturnsExactlyOneResultWithAmpersandInTitle = function (url, callback) {
+				var error = null,
+					response = {
+						statusCode: 200
+					},
+					body = `
+<div class="nw-c-most-read__items gel-layout gel-layout--no-flex">
+    <ol class="gel-layout__item">
+        <li data-entityid="most-popular-read-1" class="gel-layout__item gs-o-faux-block-link gs-u-mb+ gel-1/2@m gel-1/5@xxl gs-u-float-left@m gs-u-clear-left@m gs-u-float-none@xxl"><span class="gs-o-media"><span class="nw-c-most-read__rank gs-o-media__img gel-canon gel-1/12@xs gel-1/8@m gel-1/10@l gel-2/12@xxl gs-u-align-center">1</span>
+            <a class="gs-c-promo-heading nw-o-link gs-o-bullet__text gs-o-faux-block-link__overlay-link gel-pica-bold gs-u-pl-@xs gs-o-media__body"
+                href="/news/blogs-the-papers-38565081"><span class="gs-c-promo-heading__title gel-pica-bold">Newspaper headlines: 'Broken' A&E and Streep takes on Trump</span></a>
+            </span>
+        </li>      
+    </ol>
+</div>
 		  `;
-		callback(error, response, body);
-	  }
-	  
-	  var newsParser = new NewsParser(requestThatAlwaysReturnsExactlyOneResultWithAmpersandInTitle);
-	  
-	  // act
-	  newsParser.parseMostReadArticles(function(result) {
-		  
-		// assert
-  		expect(result.length).toEqual(1);
-		expect(result[0].url).toEqual("http://www.bbc.co.uk/news/blogs-the-papers-38565081");
-		expect(result[0].title).toEqual("Newspaper headlines: 'Broken' A&E and Streep takes on Trump");
-		done();
-	  });
-    }); 
-	
-    it("bad status code, returns empty array", function(done) {
-	  // arrange 
-	  var requestThatAlwaysReturnsBadStatusCode = function(url, callback) {
-		var error = null,
-		  response = { statusCode: 500 },
-		  body = "";
-		callback(error, response, body);
-	  }
-	  
-	  var newsParser = new NewsParser(requestThatAlwaysReturnsBadStatusCode);
-	  
-	  // act
-	  newsParser.parseMostReadArticles(function(result) {  
-		  
-		// assert
-  		expect(result).toEqual([]);
-		done();
-	  });
-    }); 
-  });
+				callback(error, response, body);
+			}
+
+			var newsParser = new NewsParser(requestThatAlwaysReturnsExactlyOneResultWithAmpersandInTitle);
+
+			// act
+			newsParser.parseMostReadArticles(function (result) {
+
+				// assert
+				expect(result.length).toEqual(1);
+				expect(result[0].url).toEqual("http://www.bbc.co.uk/news/blogs-the-papers-38565081");
+				expect(result[0].title).toEqual("Newspaper headlines: 'Broken' A&E and Streep takes on Trump");
+				done();
+			});
+		});
+
+		it("bad status code, returns empty array", function (done) {
+			// arrange 
+			var requestThatAlwaysReturnsBadStatusCode = function (url, callback) {
+				var error = null,
+					response = {
+						statusCode: 500
+					},
+					body = "";
+				callback(error, response, body);
+			}
+
+			var newsParser = new NewsParser(requestThatAlwaysReturnsBadStatusCode);
+
+			// act
+			newsParser.parseMostReadArticles(function (result) {
+
+				// assert
+				expect(result).toEqual([]);
+				done();
+			});
+		});
+	});
 });
